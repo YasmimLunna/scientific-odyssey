63ddd9dcc077bd7c8c10e8201b62e66d17a9c2b1
Ludmilla Vaz <ludmillaslvaz@gmail.com>
2024-06-18 10:32:02 -0300

    add btn no html

--- js/valida-confirmaSenha.js
+++
@@ -1,7 +0,0 @@
-export function validaConfirmaSenha(campo, senha) {
-    if (campo.value !== senha) {
-        campo.setCustomValidity('As senhas não coincidem.');
-    } else {
-        campo.setCustomValidity('');
-    }
-}
